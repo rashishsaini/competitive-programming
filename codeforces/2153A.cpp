65b7fb524d6107bf44b24109cc9fbc7604cc67c4
rashishsaini <rashishsaini07@gmail.com>
2025-10-14 03:10:29 +0530

    done

---
+++ codeforces/2153A.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end());
+        int count = 1;
+        for(int i = 0; i < n-1; i++){
+            if(a[i] != a[i+1]){
+                count++;
+            }}
+        cout << count << endl;                
+
+    }
+return 0;
+}
