11c8fedc01f5adee1c8d04aac0648f5c5cc87680
rashishsaini <rashishsaini07@gmail.com>
2025-10-22 22:08:10 +0530

    ok

---
+++ codeforces/151A.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,k,l,c,d,p,nl,np,x,y,z;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    x = k*l/nl;
+    y = c*d;
+    z = p/np;
+    cout<< min(min(x,y),z)/n;
+return 0;
+}
